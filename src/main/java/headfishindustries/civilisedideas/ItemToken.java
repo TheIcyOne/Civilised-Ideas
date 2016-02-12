49e4eb23aff66210621128ddc0d5e78a25a8029e
Arcanuo <logjohn13@gmail.com>
2016-02-11 18:57:37 -0800

    Update ItemToken.java

--- src/main/java/headfishindustries/civilisedideas/ItemToken.java
+++ src/main/java/headfishindustries/civilisedideas/ItemToken.java
@@ -1,10 +1,12 @@
 package headfishindustries.civilisedideas;
 
 import net.minecraft.item.Item;
+net.minecraft.creativetab.CreativeTabs
 
 public class ItemToken extends Item {
 	public ItemToken(){
-		
-		
+	SetMaxStackSize (64);	
+	SetCreativeTab(tab.);
+	item.ItemToken.name=Swapping Token
 	}
 }
