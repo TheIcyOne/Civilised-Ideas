e4e80be43c3f83decd524f933e580ab9001ce6ae
TheIcyOne <icy.headfishindustries@gmail.com>
2016-03-25 23:13:34 +0000

    Wooh, changes
    Did things. This is exciting, now isn't it?

---
+++ src/main/java/headfishindustries/civilisedideas/commands/GetDim.java
@@ -0,0 +1,48 @@
+package headfishindustries.civilisedideas.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentText;
+
+public class GetDim extends CommandBase {
+	@Override
+	public String getCommandName(){
+		return "getDim";
+	}
+	
+	@Override
+	public String getCommandUsage(ICommandSender sender){
+		return "/getDim";
+	}
+	
+	@Override
+	public int getRequiredPermissionLevel(){
+		return 0;
+	}
+	
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender sender){
+		return true;
+	}
+	
+	@Override
+	public List getCommandAliases(){
+		ArrayList<String> aliases = new ArrayList<String>();
+		aliases.add("getdim");
+		aliases.add("GetDim");
+		aliases.add("GETDIM");
+		aliases.add("getCurrentDim");
+		aliases.add("getDimension");
+		return aliases;
+	}
+	
+	@Override
+	public void processCommand(ICommandSender sender, String[] idontneedthis){
+		EntityPlayer player = getCommandSenderAsPlayer(sender);
+		player.addChatMessage(new ChatComponentText("You are in dimension number " + player.dimension));
+	}
+}
