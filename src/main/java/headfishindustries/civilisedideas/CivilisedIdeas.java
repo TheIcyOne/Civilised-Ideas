74633f8a6b18b20b38e42d02f4fd2fb10b644649
TheIcyOne <icy.headfishindustries@gmail.com>
2016-03-25 23:13:48 +0000

    Oops

--- src/main/java/headfishindustries/civilisedideas/CivilisedIdeas.java
+++ src/main/java/headfishindustries/civilisedideas/CivilisedIdeas.java
@@ -6,9 +6,13 @@ import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import headfishindustries.civilisedideas.commands.GetDim;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.command.ICommandManager;
+import net.minecraft.command.ServerCommandManager;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
@@ -21,29 +25,24 @@ public class CivilisedIdeas {
 	@Instance(value = "ci")
 	public static CivilisedIdeas instance;
 	
-	public static Block blockSwapper;
-	
-	public static Item itemToken;
 	public static Item itemGrapple;
 	
 	public static Entity entityGrapple;
 	
 	@EventHandler
+	public void serverStarting(FMLServerStartingEvent event){
+		ICommandManager cmdManage = event.getServer().getCommandManager();
+		ServerCommandManager svrCommandManage = ((ServerCommandManager)cmdManage);
+		svrCommandManage.registerCommand(new GetDim());
+	}
+	
+	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
 		//Block and Item Initialization
-		blockSwapper = new BlockSwapper(Material.anvil).setBlockName("blockSwapper");
-		
-		itemToken = new ItemToken();
 		itemGrapple = new ItemGrapple();
 		
 		
-		
-				
-		GameRegistry.registerBlock(blockSwapper, "block_swapper");
-		
-		GameRegistry.registerItem(itemToken, "item_token");
-		
-		GameRegistry.registerTileEntity(headfishindustries.civilisedideas.TileSwapper.class, "tile_swapper");
+
 	}
 	
 
