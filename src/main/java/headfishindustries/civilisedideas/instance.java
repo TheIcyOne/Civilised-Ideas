6bf4ef8dd997c044fcf8d9cc6cbacb097a98ccc2
TheIcyOne <TheIcyOne@users.noreply.github.com>
2016-03-27 20:09:19 +0100

    Revert "1.9 init"

--- src/main/java/headfishindustries/civilisedideas/instance.java
+++
@@ -1,8 +0,0 @@
-package headfishindustries.civilisedideas;
-
-public @interface instance {
-    public static String MODID = "CivilisedIdeas";
-    public static String VERSION = "0";
-    public static String NAME ="Civilised Ideas";
-    public static String MODLANGUAGE = "java";
-}
