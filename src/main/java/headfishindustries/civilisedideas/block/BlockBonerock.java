15e0253dff27c325d90b74b2bba61d74c2717f33
TheIcyOne <icy.headfishindustries@gmail.com>
2016-06-16 16:15:12 +0100

    Added pizzablockdrops
    Now drops skeleton skulls and bones, and requires a pickaxe to harvest.

--- src/main/java/headfishindustries/civilisedideas/block/BlockBonerock.java
+++ src/main/java/headfishindustries/civilisedideas/block/BlockBonerock.java
@@ -1,16 +1,40 @@
 package headfishindustries.civilisedideas.block;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
 import headfishindustries.civilisedideas.CivilisedIdeas;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockOre;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
-public class BlockBonerock extends Block{
+public class BlockBonerock extends BlockOre{
 
 	public BlockBonerock() {
-		super(Material.rock);
+		super();
+		this.setHarvestLevel("pickaxe", 1);
 		setBlockName("ci_rockBone");
 		setBlockTextureName("ci:rockBone");
 	}
+	
+	public void dropBlockAsItemWithChance(World world, int x, int y, int z){
+		super.dropBlockAsItemWithChance(world, x, y, z, z, blockHardness, z);
+		
+	}
+	
+	@Override
+	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int meta, int fortune){
+		ArrayList<ItemStack> drop = new ArrayList<ItemStack>();
+		drop.add(new ItemStack(Items.skull, world.rand.nextInt(2) + 1 + fortune, 0));
+		drop.add(new ItemStack(Items.bone, world.rand.nextInt(4) + 2 + fortune));
+		return drop;
+	}
+
 }
